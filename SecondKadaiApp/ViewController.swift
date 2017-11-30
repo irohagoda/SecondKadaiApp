d8d9751bc1b1847c76f05868c8b0dd8934f401ea
郷田彩巴 <goda@godairoha.local>
2017-11-30 23:12:10 +0900

    二つ目のアプリ

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        resultViewController.x = textField.text!
+  
 
-
+    }
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
 }
 
